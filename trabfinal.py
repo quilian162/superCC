ec6535590e5c4fbde7e0c77bd70f18c4a6275177
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-04-11 21:16:58 -0300

    Update trabfinal.py

--- trabfinal.py
+++ trabfinal.py
@@ -4,8 +4,8 @@ class tprod:
   nome=''
   preco=0.0
  
- # funcao que retorna o preco do produto
- def retpreco(c, lprod):
+# funcao que retorna o preco do produto
+def retpreco(c, lprod):
   for i in range(len(lprod):
     if lprod[i].cod==c:
       return lprod[i].preco
@@ -30,10 +30,50 @@ def insereprod(lista):
       break
     if retpreco(p.cod, lista)!=1 # para verificar se o codigo ja foi cadastrado
       print('Código já cadastrado.) # pois se o preco for igual a 0 o produto nao existe
-      continue
+      continue # se nao existir, volta ao comeco do laço
     p.nome=input('Nome: ")
     p.preco=float(input('Preço: '))
     cadprod(p, lista) #chama a funcao de cadastro
 
 def carregarprod(lista):
-  f=open(
+  f=open('produtos.txt','a+')
+  f.seek(0)
+  llin=f.readlines()
+  for l i llin:
+    prod=l.split(',')
+    p=tprod()
+    p.cod=int(prod[0])
+    p.nome=prod[1]
+    p.preco=float(prod[2][:-'])
+    lista.append(p)
+  f.close()
+                          
+def armzprod(lista)
+  f=open('produtos.txt','w')
+  for i in range(len(lista)):
+    f.write(str(lista[i].cod)+','+lista[i].nome+','+str(lista[i].preco)+'\n')
+# inserindo manualmente os produtos na lista para utilizar a funcao que pede os produtos pelo teclado
+
+#Main do Programa
+produtos[]
+carregarprod(produtos)
+insereprod(produtos)                  
+armzprod(produtos)
+x = 'Sim'
+while x == 'Sim':
+  total=0
+  cod=int(input('Código: '))
+  while cod!=0
+    psr=0
+    qnt=int(input('Quantidade: '))
+    print(retnome(cod, produtos),' ',end='')
+    pr=retpreco(cod,produtos)
+    if pr==-1:
+      print('Produto não cadastrado')
+    else:
+      psr=pr*qt
+      total+=pr*qt
+      print('R$ {:.2f} , Valor Parcial: R$ {:.2f} '.format(psr,total))
+    cod=int(input('Código: '))
+  print('Valor final: %.2f'%total)
+  x=input('Continuar? (X(sim)/Y(nao): ').upper()
